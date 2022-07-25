6fd5af9e1eecb584a1a4a0c9aad8a3fdd28e550c
Replit user <>
2022-07-25 13:26:56 +0000

    exporting freecodecamp  files to github

--- myApp.js
+++ myApp.js
@@ -1,5 +1,24 @@
 let express = require('express');
 let app = express();
+// inserting public files
+app.use('/public',express.static(__dirname + '/public'))
+// setting the route
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname +'/views/index.html');
+})
+// serving a json 
+app.get('/json',(req,res)=>{
+
+    console.log(process.env.MESSAGE_STYLE)
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+    res.json({"message": "Hello Json"})
+  }else{
+    console.log('goodbye')
+    res.json({"message": "HELLO JSON"})
+  }
+})
+// use .env
+
 
 
 
