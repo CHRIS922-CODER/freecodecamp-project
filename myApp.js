a1e710915199df389c265618f2c63dd93ea0dac5
christopher <christopheromosa632@gmail.com>
2022-07-26 11:10:00 +0300

    setting up the dotenv dependencies

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 // inserting public files
 app.use('/public',express.static(__dirname + '/public'))
 // setting the route
@@ -11,10 +12,10 @@ app.get('/json',(req,res)=>{
 
     console.log(process.env.MESSAGE_STYLE)
   if(process.env.MESSAGE_STYLE === 'uppercase'){
-    res.json({"message": "Hello Json"})
+    res.json({"message": "HELLO JSON"})
   }else{
     console.log('goodbye')
-    res.json({"message": "HELLO JSON"})
+    res.json({"message": "Hello json"})
   }
 })
 // use .env
